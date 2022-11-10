948cbca7dfc0bda954dbb4fd058cae9f63998e27
Omar Hussein <omarmohammad1951@gmail.com>
2022-11-09 19:08:23 -0500

    organization

--- server/api/models/index.js
+++
@@ -1,5 +0,0 @@
-import Users from './user.js';
-import Passwords from './password.js';
-import { AccessTokens, RefreshTokens } from './token.js';
-
-export { Users, Passwords, AccessTokens, RefreshTokens };
--- server/api/models/password.js
+++
@@ -1,8 +0,0 @@
-import { Schema, model } from 'mongoose';
-
-const PasswordSchema = new Schema({
-  _id: { type: String },
-  hash: { type: String, required: true }
-});
-
-export default model('Passwords', PasswordSchema);
