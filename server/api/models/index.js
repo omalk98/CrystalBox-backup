b2108c889099d4550a7e1d6890e40b723accb419
Philippe Cormier <phili.C@hotmail.com>
2022-11-09 19:12:35 -0500

    Added user models and populate-db.js

---
+++ server/api/models/index.js
@@ -0,0 +1,5 @@
+import Users from './user.js';
+import Passwords from './password.js';
+import { AccessTokens, RefreshTokens } from './token.js';
+
+export { Users, Passwords, AccessTokens, RefreshTokens };
