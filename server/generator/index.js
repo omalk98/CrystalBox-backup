a945019e935e2d7230546e8e1e2470d623e64c9b
Omar H <omarmohammad1951@gmail.com>
2022-12-01 22:52:35 -0500

    updated server packages

--- server/generator/index.js
+++ server/generator/index.js
@@ -1,3 +1,10 @@
 import generateUsers from './modules/generateUsers.js';
 
-generateUsers(2000);
+const count = parseInt(process.argv[2], 10);
+
+if (!count) {
+  console.error('Please provide a number of users to generate');
+  process.exit(1);
+}
+
+generateUsers(count);
