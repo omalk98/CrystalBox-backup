164a5725a82433d446d3f8d0256f2ef3c7ee3417
Omar H <omarmohammad1951@gmail.com>
2022-12-04 17:35:01 -0500

    added extra reset-password route protection

--- server/api/routes/view.js
+++ server/api/routes/view.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 import { mainPage, forgotPassword } from '../controllers/view.js';
+import { validatePasswordLink } from '../middleware/index.js';
 
 const viewRouter = Router();
 
-viewRouter.get('/user/forgot-password/:id', forgotPassword);
+viewRouter.get(
+  '/user/forgot-password/:id',
+  validatePasswordLink,
+  forgotPassword
+);
 
 viewRouter.get('*', mainPage);
 
