8f1e9f9d4f2c8b2f7a392bf21446df904be31332
Omar H <omarmohammad1951@gmail.com>
2022-12-04 01:26:02 -0500

    fixed some style issues in the code

--- server/api/routes/content.js
+++ server/api/routes/content.js
@@ -1,10 +1,10 @@
 import { Router } from 'express';
+import { homeController, footerController } from '../controllers/content.js';
+
 const contentRouter = Router();
 
 const baseAPI = '/api/v1';
 const contentAPI = `${baseAPI}/content`;
-
-import { homeController, footerController } from '../controllers/content.js';
 
 contentRouter.get(`${contentAPI}/home`, homeController);
 
