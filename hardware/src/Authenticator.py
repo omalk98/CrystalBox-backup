4513995f24ab55282eb70edd17e70032dd2bb238
Omar Hussein <omarmohammad1951@gmail.com>
2023-01-26 16:07:43 -0500

    added functionality to authenticator class

--- hardware/src/Authenticator.py
+++ hardware/src/Authenticator.py
@@ -8,6 +8,7 @@ class Authenticator:
     def __init__(self, gate_id) -> None:
         self.gate_id = gate_id
 
+    # Fetch a URL from the server using base_url
     def fetch(self, url):
         try:
             response = urlopen(f"{self.base_url}{url}")
@@ -16,9 +17,23 @@ class Authenticator:
             print("WARNING: Could NOT Connect to Server.")
             return None
 
-    def validateKeyAccess(self, key, uuid) -> bool:
+    # Validate an RFID tag's access permissions
+    def validateTagAccess(self, key, uuid) -> bool:
         url = f"/access/{self.gate_id}/{key}/{uuid}"
         response = self.fetch(url)
         if response == None or response.getcode() != 200:
+            print("WARNING: Tag does not have appropriate access permissions.")
             return False
         return True
+    
+    # Replace a User's RFID tag with a new one
+    def replaceTag(self, user_id, new_key) -> str:
+        url = f"/replace/{self.gate_id}/{user_id}/{new_key}"
+        response = self.fetch(url)
+        if response == None or response.getcode() != 200:
+            print("WARNING: Failed to replace tag.")
+            return None
+        new_uuid = json.loads(response.read().decode("utf-8"))["new_uuid"]
+        return new_uuid
+
+    
--- hardware/src/Effects.py
+++ hardware/src/Effects.py
@@ -7,34 +7,47 @@ class Effects:
         self.buzzer = Buzzer(buzzer)
         self.led = LED(led)
 
+    # Turn off the buzzer and LED
     def off(self) -> None:
         self.led.off()
         self.buzzer.off()
     
-    def success_on(self) -> None:
+    # Set the LED and buzzer to their success mode
+    def success_mode(self) -> None:
         self.led.success()
         self.buzzer.success()
 
-    def failure_on(self) -> None:
+    # Set the LED and buzzer to their failure mode
+    def failure_mode(self) -> None:
         self.led.failure()
         self.buzzer.failure()
+    
+    # Set the LED and buzzer to their standby mode
+    def standbyMode(self) -> None:
+        self.led.standby()
+        self.buzzer.off()
+
+    # Set the LED and buzzer to their processing mode
+    def processingMode(self) -> None:
+        self.led.processing()
+        self.buzzer.off()
 
     # Play a success sound and light the LED green
     def success(self) -> None:
-        self.success_on()
+        self.successMode()
         sleep(0.05)
         self.off()
         sleep(0.05)
-        self.success_on()
+        self.successMode()
         sleep(0.1)
         self.off()
 
     # Play a failure sound and light the LED red
     def failure(self) -> None:
-        self.failure_on()
+        self.failureMode()
         sleep(0.2)
         self.off()
         sleep(0.01)
-        self.failure_on()
+        self.failureMode()
         sleep(0.2)
         self.off()
