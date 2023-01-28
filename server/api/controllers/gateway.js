e78313b9c3f2ea4f1a611edf37ab5b69126a8df3
Omar Hussein <omarmohammad1951@gmail.com>
2023-01-28 02:06:16 -0500

    network module setup started

--- server/api/controllers/gateway.js
+++ server/api/controllers/gateway.js
@@ -1,31 +1,44 @@
 const validateUserAccess = (req, res) => {
   const { Gate_ID } = req.headers;
-  const { id } = req.params;
-  console.log(`Gateway ${Gate_ID} is up and running with user ${id}!`);
-  if (!id || id !== '123') {
-    res.status(400).send('Bad request');
-    return;
-  }
+  const { key, uuid } = req.params;
+  console.log('getUserDetails');
+  console.log(`key: ${key}`);
+  console.log(`uuid: ${uuid}`);
+
+  // will be used in middleware
+  console.log(`Gate_ID: ${Gate_ID}`);
   res.status(200).send('Hello from the gateway!');
 };
 
 const getUserDetails = async (req, res) => {
+  const { key, uuid } = req.params;
   console.log('getUserDetails');
+  console.log(`key: ${key}`);
+  console.log(`uuid: ${uuid}`);
   res.sendStatus(200);
 };
 
 const createUserTag = async (req, res) => {
+  const { key, uuid } = req.params;
   console.log('createUserTag');
+  console.log(`key: ${key}`);
+  console.log(`uuid: ${uuid}`);
   res.sendStatus(200);
 };
 
 const replaceUserTag = async (req, res) => {
+  const { key, uuid } = req.params;
   console.log('replaceUserTag');
+  console.log(`key: ${key}`);
+  console.log(`uuid: ${uuid}`);
   res.sendStatus(200);
 };
 
 const removeUserTag = async (req, res) => {
+  const { key, uuid } = req.params;
   console.log('removeUserTag');
+  console.log(`key: ${key}`);
+  console.log(`uuid: ${uuid}`);
   res.sendStatus(200);
 };
 
